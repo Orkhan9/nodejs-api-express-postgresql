c083934a517ee71e0b3ae107bb4a06c833542b5d
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-03-29 00:57:14 +0400

    update todo

--- nodejs-postgresql/index.js
+++ nodejs-postgresql/index.js
@@ -14,6 +14,23 @@ app.get('/todos',async (req,res)=>{
         console.error(e.message)
     }
 })
+//get a todo
+app.get('/todos/:id',async (req,res)=>{
+    // console.log(req.params)
+    const {id}=req.params;
+    try {
+        const todo=await pool.query('SELECT * FROM todo WHERE todo_id=$1',[id]);
+        console.log(todo.rows)
+        if(todo.rows.length>0){
+            res.json(todo.rows[0]);
+        }else{
+            res.json(`${id} - id todo is not existed`)
+        }
+
+    }catch (e) {
+        console.error(e.message);
+    }
+})
 //create a todo
 app.post('/todos',async (req,res)=>{
     try{
@@ -28,8 +45,27 @@ app.post('/todos',async (req,res)=>{
     }
 })
 //update a todo
-
+app.put('/todos/:id',async (req,res)=>{
+    const {id}=req.params;
+    const {description}=req.body;
+    try {
+        const updateTodo=await pool.query('UPDATE todo SET description=$1 WHERE todo_id=$2',[description,id]);
+        // res.json('todo was updated');
+        res.json('todo was updated');
+    }catch (e) {
+        console.error(e.message);
+    }
+})
 //delete a todo
+app.delete('/todos/:id',async (req,res)=>{
+    const {id}=req.params;
+    try {
+        const deleteTodo=await pool.query('DELETE FROM todo WHERE todo_id=$1',[id])
+        res.json(`${id}-id todo was deleted`);
+    }catch (e) {
+        console.error(e.message);
+    }
+})
 
 app.listen(3000,()=>{
     console.log('server is listening on port 3000')
